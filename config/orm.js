379ed04f1755edd6b5bdd6814a7b3c6e2ec1ded3
Ravalash <13k0873juhky@opayq.net>
2020-05-16 22:54:46 -0400

    Updating SQL functions

---
+++ config/orm.js
@@ -0,0 +1,86 @@
+const connection = require("./connection.js");
+
+const orm = {
+  endConnection: () => {
+    connection.end();
+  },
+
+  selectAsync: (tableCol, tableName, sortCol) => {
+    return new Promise((resolve, reject) => {
+      const queryString = "SELECT ?? FROM ?? ORDER BY ??";
+      connection.query(
+        queryString,
+        [tableCol, tableName, sortCol],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+  },
+
+  selectWhereAsync: (tableCol, tableName, colName, colValue, sortCol) => {
+    return new Promise((resolve, reject) => {
+      const queryString = "SELECT ?? FROM ?? WHERE ?? = ? ORDER BY ??";
+      connection.query(
+        queryString,
+        [tableCol, tableName, colName, colValue, sortCol],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+  },
+
+  selectDeptBudget: (deptID) => {
+    return new Promise((resolve, reject) => {
+      const queryString =
+        "select department.id, sum(salary) AS Salary, count(employee.id) as Employees , department.name AS 'Department Name' from employee join role on employee.role_id = role.id join department on role.department_id = department.id where department.id = ?";
+      connection.query(queryString, [deptID], (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+  },
+
+  createAsync: (tableName, colName, colValue) => {
+    return new Promise((resolve, reject) => {
+      const queryString = "INSERT INTO ?? (??) VALUES ?";
+      connection.query(
+        queryString,
+        [tableName, colName, colValue],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+  },
+
+  updateAsync: (tableName, colName, colValue, recordID) => {
+    return new Promise((resolve, reject) => {
+      const queryString = "update ?? SET ?? = ? where id = ?";
+      connection.query(
+        queryString,
+        [tableName, colName, colValue, recordID],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+  },
+  
+  deleteAsync: (tableName, recordID) => {
+    return new Promise((resolve, reject) => {
+      const queryString = "DELETE FROM ?? WHERE id = ?";
+      connection.query(queryString, [tableName, recordID], (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+  },
+};
+
+module.exports = orm;
